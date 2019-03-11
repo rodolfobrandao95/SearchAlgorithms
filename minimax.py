945a7ba518a16760b7c0cc9d71baf5200989ee96
Rodolfo Brandão <23040936+rodolfobrandao95@users.noreply.github.com>
2019-03-11 20:24:09 -0300

    Update minimax.py

--- minimax.py
+++ minimax.py
@@ -1,29 +1,15 @@
 def minimax(arr, depth):
-    if (depth % 2 == 0):
-        return get_max(arr)
+    if depth == 1:
+        return print(arr[0])
     else:
-        return get_min(arr)
+        winners = []
 
+        if depth % 2 == 0:
+            for i in range(0, len(arr), 2):
+                winners.append(max(arr[i], arr[i + 1]))
+        else:
+            for i in range(0, len(arr), 2):
+                winners.append(min(arr[i], arr[i + 1]))
 
-def get_max(arr):
-    if (len(arr) == 1):
-        return arr[0]
-    else:
-        max_winners = []
-
-        for i in range(0, len(arr), 2):
-            max_winners.append(max(arr[i], arr[i + 1]))
-
-    return get_min(max_winners)
-
-
-def get_min(arr):
-    if(len(arr) == 1):
-        return arr[0]
-    else:
-        min_winners = []
-
-        for i in range(0, len(arr), 2):
-            min_winners.append(min(arr[i], arr[i + 1]))
-
-    return get_max(min_winners)
+        depth -= 1
+        minimax(winners, depth)
